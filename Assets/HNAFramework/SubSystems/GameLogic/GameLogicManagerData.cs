4997e384031642d34c460feba29cf9ac27f2f9bc
snowysmile <snowy_smile@qq.com>
2018-01-27 19:52:38 +0800

    GameLogicManager Change

--- Assets/HNAFramework/SubSystems/GameLogic/GameLogicManagerData.cs
+++ Assets/HNAFramework/SubSystems/GameLogic/GameLogicManagerData.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game Jam/Game Logic Data")]
 public class GameLogicManagerData : ScriptableObject
 {
-    public float hp = 4;
-    public float speed = 1;
-    public float size; 
+    public float hp;
+    public float size;
+
+    //速度相关的所有变量
+    public float speed;
+    public float topDashTime;
+    public float botDashTime;
 }
